d3e263bc5deaaad45892c56fe25bfefa25f29c11
manasa <manasa@gmail.com>
2023-04-28 10:05:08 +0000

    nxtWatch

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,133 +1,437 @@
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
-import CourseItem from '../CourseItem'
+
+import {AiFillHome, AiOutlineSearch} from 'react-icons/ai'
+import {HiFire} from 'react-icons/hi'
+import {GiLoveMystery} from 'react-icons/gi'
+import {RiMenuAddFill, RiCloseLine} from 'react-icons/ri'
+import Cookies from 'js-cookie'
+
+import VideoItem from '../VideoItem'
 import Header from '../Header'
 
+import VideoContext from '../../context/VideoContext'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
 import {
-  BgContainer,
-  ErrorContainer,
-  ErrorImg,
-  ErrorHeading,
-  ErrorInfo,
-  ErrorRetryButton,
-  CourseItemsList,
-  CoursesListContainer,
-  Heading,
+  HomeRouteContainer,
+  HomeContainer,
+  SideBarContainer,
+  NavLinksContainer,
+  LinkGenerate,
+  LinkContainer,
+  Nav,
+  NavHome,
+  ContactUsContainer,
+  ContactUs,
+  LogosContainer,
+  Logo,
+  ContactUsTagLine,
+  HomeBarContainer,
+  BannerContainer,
+  NxtLogo,
+  LogoWithCloseBtn,
+  CloseBtn,
+  BannerDescription,
+  GetItNow,
+  SearchContainer,
+  SearchInput,
+  IconSearchBtn,
+  HomeVideosContainer,
+  LinkContainerHome,
+  SideBarContainerDark,
+  LinkContainerHomeDark,
+  NavHomeDark,
+  NavDark,
+  ContactUsDark,
+  ContactUsTagLineDark,
+  HomeBarContainerDark,
+  BannerContainerDark,
+  SearchContainerDark,
+  SearchInputDark,
+  IconSearchBtnDark,
   LoaderContainer,
+  FailureContainer,
+  FailureImage,
+  FailureName,
+  FailureDescription,
+  RetryBtn,
+  NoResultsContainer,
+  NoResultsImage,
+  NoResultsName,
+  NoResultsDescription,
 } from './styledComponents'
 
-const apiStatusConstants = {
+const apiConstants = {
   initial: 'INITIAL',
   success: 'SUCCESS',
+  progress: 'PROGRESS',
   failure: 'FAILURE',
-  inProgress: 'IN_PROGRESS',
 }
 
 class Home extends Component {
   state = {
-    apiStatus: apiStatusConstants.initial,
-    courseList: [],
+    searchInput: '',
+    homeVideos: [],
+    apiStatus: apiConstants.initial,
+    bannerDisplay: true,
   }
 
   componentDidMount() {
-    this.getCourses()
+    this.getHomeVideos()
   }
 
-  getCourses = async () => {
+  getHomeVideos = async () => {
     this.setState({
-      apiStatus: apiStatusConstants.inProgress,
+      apiStatus: apiConstants.progress,
     })
-    const url = 'https://apis.ccbp.in/te/courses'
+    const {searchInput} = this.state
+    const jwtToken = Cookies.get('jwt_token')
+    const url = `https://apis.ccbp.in/videos/all?search=${searchInput}`
     const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
       method: 'GET',
     }
     const response = await fetch(url, options)
-    if (response.ok) {
-      const fetchedData = await response.json()
-      const updatedData = fetchedData.courses.map(each => ({
-        id: each.id,
-        name: each.name,
-        logoUrl: each.logo_url,
-      }))
+    if (response.ok === true) {
+      const data = await response.json()
+      console.log(data)
+      const updatedData = data.videos.map(each => {
+        const {channel} = each
+        return {
+          name: channel.name,
+          profileImageUrl: channel.profile_image_url,
+          id: each.id,
+          publishedAt: each.published_at,
+          thumbnailUrl: each.thumbnail_url,
+          title: each.title,
+          viewCount: each.view_count,
+          isSaved: false,
+        }
+      })
       this.setState({
-        courseList: updatedData,
-        apiStatus: apiStatusConstants.success,
+        homeVideos: updatedData,
+        apiStatus: apiConstants.success,
       })
     } else {
       this.setState({
-        apiStatus: apiStatusConstants.failure,
+        apiStatus: apiConstants.failure,
       })
+      console.log('failed fetch')
     }
   }
 
-  onClickRetry = () => {
-    this.getCourses()
+  onChangeSearch = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
   }
 
-  renderFailureView = () => (
-    <ErrorContainer>
-      <ErrorImg
-        src="https://assets.ccbp.in/frontend/react-js/tech-era/failure-img.png"
-        alt="failure view"
-      />
-      <ErrorHeading>Oops! Something Went Wrong</ErrorHeading>
-      <ErrorInfo>
-        We cannot seem to find the page you are looking for.
-      </ErrorInfo>
-      <ErrorRetryButton
-        type="button"
-        className="retry-button"
-        onClick={this.onClickRetry}
-      >
-        Retry
-      </ErrorRetryButton>
-    </ErrorContainer>
+  sideBarDark = () => (
+    <SideBarContainerDark>
+      <NavLinksContainer>
+        <LinkGenerate to="/">
+          <LinkContainerHomeDark>
+            <AiFillHome color="red" size="16" />
+            <NavHomeDark>Home</NavHomeDark>
+          </LinkContainerHomeDark>
+        </LinkGenerate>
+        <LinkGenerate to="/trending">
+          <LinkContainer>
+            <HiFire color="#64748b" size="16" />
+            <NavDark>Trending</NavDark>
+          </LinkContainer>
+        </LinkGenerate>
+        <LinkGenerate to="/gaming">
+          <LinkContainer>
+            <GiLoveMystery color="#64748b" size="16" />
+            <NavDark>Gaming</NavDark>
+          </LinkContainer>
+        </LinkGenerate>
+        <LinkGenerate to="/saved-videos">
+          <LinkContainer>
+            <RiMenuAddFill color="#64748b" size="16" />
+            <NavDark>Saved videos</NavDark>
+          </LinkContainer>
+        </LinkGenerate>
+      </NavLinksContainer>
+      <ContactUsContainer>
+        <ContactUsDark>CONTACT US</ContactUsDark>
+        <LogosContainer>
+          <Logo
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png"
+            alt="facebook logo"
+          />
+          <Logo
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-twitter-logo-img.png"
+            alt="twitter logo"
+          />
+          <Logo
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png"
+            alt="linked in logo"
+          />
+        </LogosContainer>
+        <ContactUsTagLineDark>
+          Enjoy! Now to see your channels and recommendations!
+        </ContactUsTagLineDark>
+      </ContactUsContainer>
+    </SideBarContainerDark>
   )
 
-  renderCoursesList = () => {
-    const {courseList} = this.state
+  sideBarLight = () => (
+    <SideBarContainer>
+      <NavLinksContainer>
+        <LinkGenerate to="/">
+          <LinkContainerHome>
+            <AiFillHome color="red" size="16" />
+            <NavHome>Home</NavHome>
+          </LinkContainerHome>
+        </LinkGenerate>
+        <LinkGenerate to="/trending">
+          <LinkContainer>
+            <HiFire color="#64748b" size="16" />
+            <Nav>Trending</Nav>
+          </LinkContainer>
+        </LinkGenerate>
+        <LinkGenerate to="/gaming">
+          <LinkContainer>
+            <GiLoveMystery color="#64748b" size="16" />
+            <Nav>Gaming</Nav>
+          </LinkContainer>
+        </LinkGenerate>
+        <LinkGenerate to="/saved-videos">
+          <LinkContainer>
+            <RiMenuAddFill color="#64748b" size="16" />
+            <Nav>Saved videos</Nav>
+          </LinkContainer>
+        </LinkGenerate>
+      </NavLinksContainer>
+      <ContactUsContainer>
+        <ContactUs>CONTACT US</ContactUs>
+        <LogosContainer>
+          <Logo
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png"
+            alt="facebook logo"
+          />
+          <Logo
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-twitter-logo-img.png"
+            alt="twitter logo"
+          />
+          <Logo
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png"
+            alt="linked in logo"
+          />
+        </LogosContainer>
+        <ContactUsTagLine>
+          Enjoy! Now to see your channels and recommendations!
+        </ContactUsTagLine>
+      </ContactUsContainer>
+    </SideBarContainer>
+  )
 
-    return (
-      <CoursesListContainer>
-        <Heading>Courses</Heading>
-        <CourseItemsList>
-          {courseList.map(each => (
-            <CourseItem key={each.id} courseItemDetails={each} />
-          ))}
-        </CourseItemsList>
-      </CoursesListContainer>
-    )
+  clickCloseIcon = () => {
+    this.setState(prevState => ({
+      bannerDisplay: !prevState.bannerDisplay,
+    }))
   }
+
+  clickSearch = () => {
+    this.getHomeVideos()
+  }
+
+  clickRetryBtn = () => {
+    this.getHomeVideos()
+  }
+
+  renderFailureApi = () => (
+    <VideoContext.Consumer>
+      {value => {
+        const {isDark} = value
+        const image = isDark
+          ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-dark-theme-img.png'
+          : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-light-theme-img.png'
+        return (
+          <FailureContainer dark={isDark}>
+            <FailureImage src={image} alt="failure view" />
+            <FailureName dark={isDark}>Oops! Something Went Wrong</FailureName>
+            <FailureDescription dark={isDark}>
+              We are having some trouble to complete your request. Please Try
+              again.
+            </FailureDescription>
+            <RetryBtn type="button" onClick={this.clickRetryBtn}>
+              Retry
+            </RetryBtn>
+          </FailureContainer>
+        )
+      }}
+    </VideoContext.Consumer>
+  )
 
   renderLoader = () => (
     <LoaderContainer data-testid="loader">
-      <Loader type="ThreeDots" color="#00BFFF" height={50} width={50} />
+      <Loader type="ThreeDots" color="blue" height="70" width="70" />
     </LoaderContainer>
   )
 
-  renderCourses = () => {
-    const {apiStatus} = this.state
+  renderNoSearchResultsView = () => (
+    <VideoContext.Consumer>
+      {value => {
+        const {isDark} = value
+        return (
+          <NoResultsContainer>
+            <NoResultsImage
+              src="https://assets.ccbp.in/frontend/react-js/nxt-watch-no-search-results-img.png"
+              alt="no videos"
+            />
+            <NoResultsName dark={isDark}>No Search results found</NoResultsName>
+            <NoResultsDescription dark={isDark}>
+              Try different key words or remove search filter
+            </NoResultsDescription>
+            <RetryBtn type="button" onClick={this.clickRetryBtn}>
+              Retry
+            </RetryBtn>
+          </NoResultsContainer>
+        )
+      }}
+    </VideoContext.Consumer>
+  )
 
+  HomeVideosContainer = () => {
+    const {homeVideos} = this.state
+    return homeVideos.length > 0 ? (
+      <HomeVideosContainer>
+        {homeVideos.map(eachVideoItem => (
+          <VideoItem video={eachVideoItem} key={eachVideoItem.id} />
+        ))}
+      </HomeVideosContainer>
+    ) : (
+      this.renderNoSearchResultsView()
+    )
+  }
+
+  renderVideosOnStatus = () => {
+    const {apiStatus} = this.state
     switch (apiStatus) {
-      case apiStatusConstants.success:
-        return this.renderCoursesList()
-      case apiStatusConstants.failure:
-        return this.renderFailureView()
-      case apiStatusConstants.inProgress:
+      case apiConstants.success:
+        return this.HomeVideosContainer()
+      case apiConstants.progress:
         return this.renderLoader()
+      case apiConstants.failure:
+        return this.renderFailureApi()
       default:
         return null
     }
   }
 
-  render() {
+  renderHomeBarDark = () => {
+    const {searchInput, bannerDisplay} = this.state
+    const display = bannerDisplay ? 'flex' : 'none'
+    console.log(searchInput)
     return (
-      <BgContainer>
-        <Header />
-        {this.renderCourses()}
-      </BgContainer>
+      <HomeBarContainerDark>
+        <BannerContainerDark data-testid="banner" display={display}>
+          <LogoWithCloseBtn>
+            <NxtLogo
+              src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+              alt="nxt watch logo"
+            />
+            <CloseBtn
+              data-testid="close"
+              type="button"
+              onClick={this.clickCloseIcon}
+            >
+              <RiCloseLine size="23" />
+            </CloseBtn>
+          </LogoWithCloseBtn>
+          <BannerDescription>
+            Buy Nxt Watch Premium prepaid plans with UPI
+          </BannerDescription>
+          <GetItNow type="button">GET IT NOW</GetItNow>
+        </BannerContainerDark>
+        <SearchContainerDark>
+          <SearchInputDark
+            type="search"
+            value={searchInput}
+            onChange={this.onChangeSearch}
+            placeholder="Search"
+          />
+          <IconSearchBtnDark
+            data-testid="searchButton"
+            onClick={this.clickSearch}
+          >
+            <AiOutlineSearch color="#cccccc" size="20" />
+          </IconSearchBtnDark>
+        </SearchContainerDark>
+        {this.renderVideosOnStatus()}
+      </HomeBarContainerDark>
     )
   }
-}
 
+  renderHomeBarLight = () => {
+    const {searchInput, bannerDisplay} = this.state
+    const display = bannerDisplay ? 'flex' : 'none'
+    console.log(searchInput)
+    return (
+      <HomeBarContainer>
+        <BannerContainer data-testid="banner" display={display}>
+          <LogoWithCloseBtn>
+            <NxtLogo
+              src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+              alt="nxt watch logo"
+            />
+            <CloseBtn
+              data-testid="close"
+              type="button"
+              onClick={this.clickCloseIcon}
+            >
+              <RiCloseLine size="23" />
+            </CloseBtn>
+          </LogoWithCloseBtn>
+          <BannerDescription>
+            Buy Nxt Watch Premium prepaid plans with UPI
+          </BannerDescription>
+          <GetItNow type="button">GET IT NOW</GetItNow>
+        </BannerContainer>
+        <SearchContainer>
+          <SearchInput
+            type="search"
+            value={searchInput}
+            onChange={this.onChangeSearch}
+            placeholder="Search"
+          />
+          <IconSearchBtn data-testid="searchButton" onClick={this.clickSearch}>
+            <AiOutlineSearch size="20" />
+          </IconSearchBtn>
+        </SearchContainer>
+        {this.renderVideosOnStatus()}
+      </HomeBarContainer>
+    )
+  }
+
+  renderHome = () => (
+    <HomeRouteContainer data-testid="home">
+      <Header />
+      <HomeContainer>
+        <VideoContext.Consumer>
+          {value => {
+            const {isDark} = value
+            return (
+              <>
+                {isDark ? this.sideBarDark() : this.sideBarLight()}
+                {isDark ? this.renderHomeBarDark() : this.renderHomeBarLight()}
+              </>
+            )
+          }}
+        </VideoContext.Consumer>
+      </HomeContainer>
+    </HomeRouteContainer>
+  )
+
+  render() {
+    return this.renderHome()
+  }
+}
 export default Home
--- src/components/SavedVideos/styledComponents.js
+++ src/components/SavedVideos/styledComponents.js
@@ -2,7 +2,12 @@ import styled from 'styled-components/macro'
 
 import {Link} from 'react-router-dom'
 
-export const NotFoundContainer = styled.div`
+export const SavedVideosRouteContainer = styled.div`
+  background-color: ${props => (props.dark ? '#0f0f0f' : '#f9f9f9')};
+  min-height: 100vh;
+`
+
+export const SavedContainer = styled.div`
   min-height: 90vh;
   display: flex;
   flex-direction: row;
@@ -11,7 +16,7 @@ export const NotFoundContainer = styled.div`
 
 export const SideBarContainer = styled.div`
   display: none;
-  background-color: #ffffff;
+  background-color: #fffffff;
   @media (min-width: 768px) {
     height: 90vh;
     width: 25vw;
@@ -37,7 +42,7 @@ export const LinkContainer = styled.div`
   padding-left: 15px;
   cursor: pointer;
 `
-export const LinkContainerTrend = styled.div`
+export const LinkContainerSaved = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -54,7 +59,7 @@ export const Nav = styled.p`
   font-weight: bold;
   margin-left: 23px;
 `
-export const NavTrend = styled.p`
+export const NavSaved = styled.p`
   color: #0f0f0f;
   font-size: 16px;
   font-family: 'Roboto';
@@ -91,10 +96,11 @@ export const ContactUsTagLine = styled.p`
   font-weight: bold;
   line-height: 1.3;
 `
+
 /** SideBarContainerDark */
 export const SideBarContainerDark = styled.div`
   display: none;
-  background-color: #0f0f0f;
+  background-color: #181818;
   @media (min-width: 768px) {
     height: 90vh;
     width: 25vw;
@@ -104,6 +110,22 @@ export const SideBarContainerDark = styled.div`
     flex-direction: column;
     justify-content: space-between;
   }
+`
+export const LinkContainerSavedDark = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 35px;
+  padding-left: 15px;
+  cursor: pointer;
+  background: #383838;
+`
+export const NavSavedDark = styled.p`
+  color: #ffffff;
+  font-size: 16px;
+  font-family: 'Roboto';
+  font-weight: bold;
+  margin-left: 23px;
 `
 export const NavDark = styled.p`
   color: #ebebeb;
@@ -127,16 +149,12 @@ export const ContactUsTagLineDark = styled.p`
   font-weight: bold;
   line-height: 1.3;
 `
-/** Not-found Bar */
+/** Saved Bar */
 
-export const NotFoundBarContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 90vh;
+export const SavedBarContainer = styled.div`
+  height: 90vh;
   width: 100vw;
-  background-color: ${props => (props.dark ? '#0f0f0f' : ' #f9f9f9')};
+  background-color: #f9f9f9;
   @media (min-width: 768px) {
     width: 75vw;
   }
@@ -144,15 +162,118 @@ export const NotFoundBarContainer = styled.div`
     width: 100%;
   }
 `
-export const NotFoundImage = styled.img`
-  width: 350px;
+export const SavedLogoAndNameContainer = styled.div`
+  height: 90px;
+  background-color: #f1f1f1;
+  display: flex;
+  align-items: center;
+  padding-left: 39px;
+  padding-right: 39px;
+  @media (max-width: 768px) {
+    height: 75px;
+    padding-left: 11px;
+    padding-right: 11px;
+  }
+`
+export const SavedLogoContainer = styled.div`
+  background-color: #cbd5e1;
+  width: 70px;
+  height: 70px;
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  @media (max-width: 768px) {
+    height: 50px;
+    width: 50px;
+    border-radius: 150px;
+  }
+`
+export const SavedVidName = styled.h1`
+  color: #0f0f0f;
+  font-size: 25px;
+  font-family: 'Roboto';
+  font-weight: bold;
+  line-height: 1.3;
+  margin-left: 15px;
+  @media (max-width: 768px) {
+    font-size: 19px;
+  }
+`
+
+/** saved Bar Dark */
+
+export const SavedBarContainerDark = styled.div`
+  min-height: 90vh;
+  width: 100vw;
+  background-color: #0f0f0f;
+  @media (min-width: 768px) {
+    width: 75vw;
+  }
+  @media (min-width: 968px) {
+    width: 100%;
+  }
+`
+export const SavedLogoAndNameContainerDark = styled.div`
+  height: 90px;
+  background-color: #212121;
+  display: flex;
+  align-items: center;
+  padding-left: 39px;
+  padding-right: 39px;
+  @media (max-width: 768px) {
+    height: 75px;
+    padding-left: 11px;
+    padding-right: 11px;
+  }
+`
+export const SavedVidNameDark = styled.h1`
+  color: #ffffff;
+  font-size: 25px;
+  font-family: 'Roboto';
+  font-weight: bold;
+  line-height: 1.3;
+  margin-left: 15px;
+  @media (max-width: 768px) {
+    font-size: 19px;
+  }
+`
+export const SavedVideosContainer = styled.ul`
+  list-style-type: none;
+  padding-left: 0px;
+  padding-left: 39px;
+  padding-right: 39px;
+  @media (max-width: 768px) {
+    padding: 11px;
+  }
+`
+/** No Saved Videos View */
+
+export const NoSavedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+  width: 100vw;
+  background-color: ${props => (props.dark ? '#0f0f0f' : '#ffffff')};
+  @media (min-width: 768px) {
+    width: 75vw;
+  }
+  @media (min-width: 968px) {
+    width: 100%;
+  }
+`
+export const NoSavedImage = styled.img`
+  width: 400px;
   height: 400px;
   @media (max-width: 768px) {
     width: 220px;
     height: 220px;
   }
 `
-export const NotFoundName = styled.h1`
+export const NoSavedName = styled.h1`
   color: ${props => (props.dark ? '#ffffff' : '#181818')};
   font-family: 'Roboto';
   font-weight: bold;
@@ -163,7 +284,7 @@ export const NotFoundName = styled.h1`
     font-size: 16px;
   }
 `
-export const NotFoundDescription = styled.p`
+export const NoSavedDescription = styled.p`
   color: ${props => (props.dark ? '#606060' : '#475569')};
   font-family: 'Roboto';
   font-weight: 400;
